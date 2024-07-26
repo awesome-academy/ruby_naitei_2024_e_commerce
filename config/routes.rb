2f665a7a1707e8c8cca7f318e35c666c8b7f0481
ThaiKhacDuoc <khacduockhamduc@gmail.com>
2024-07-26 10:35:05 +0700

    [Naitei] ft:Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :products
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   scope "(:locale)", locale: /en|vi/ do
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
     resources :users, only: %i(new create show)
     resources :account_activations, only: :edit
   end
