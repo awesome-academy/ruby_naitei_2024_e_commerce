b3a6becdaa2ec61a357764fdbe33c0fde94ac14f
Minh Huy <tran.minh.huy@sun-asterisk.com>
2024-08-01 01:05:35 +0700

    [Naitei][Feature][User] View bill list

--- app/models/bill_detail.rb
+++ app/models/bill_detail.rb
@@ -2,4 +2,6 @@ class BillDetail < ApplicationRecord
   belongs_to :product
   belongs_to :bill
   has_one :comment, dependent: :destroy
+
+  delegate :image, :name, :price, to: :product, prefix: true
 end
