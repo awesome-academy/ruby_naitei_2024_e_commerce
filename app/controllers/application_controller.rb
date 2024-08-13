876719cac70d5c04edb94d15824764dc95a6c342
ThaiKhacDuoc <khacduockhamduc@gmail.com>
2024-08-14 02:33:47 +0700

    [Naitei] Login_Signup_Gem_Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,19 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   include VouchersHelper
 
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:email, :name, :password, :password_confirmation,
+                   :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
   private
 
   def set_locale
@@ -14,13 +27,5 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
-  end
-
-  def logged_in_user
-    return if logged_in?
-
-    flash[:danger] = t "please_log_in"
-    store_location
-    redirect_to login_path
   end
 end
