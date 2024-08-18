968a536c50f654125b5e7ca2067d9f9b11e6a33b
Minh Huy <tran.minh.huy@sun-asterisk.com>
2024-08-18 23:36:42 +0700

    [Naitei][Rspec] Rspec model category

---
+++ spec/factories/categories.rb
@@ -0,0 +1,23 @@
+require "faker"
+
+FactoryBot.define do
+  factory :category do
+    name { Faker::Food.ethnic_category }
+    after(:build) do |category|
+      category.image.attach(
+        io: File.open(Rails.root.join("spec", "fixtures", "files", "test_image.png")),
+        filename: "test_image.png",
+        content_type: "image/png"
+      )
+    end
+  end
+
+  factory :comment do
+    bill_detail_id { nil }
+    user_id { nil }
+    product_id { create(:product) }
+    content { Faker::Lorem.paragraph }
+    parent_comment_id { nil }
+    star { rand(1..5) }
+  end
+end
